da718f0a99b59261f9c89df66d393d6d87fb7c74
Amrita Mishra <itizamritamishra@gmail.com>
2025-06-08 21:28:26 +0530

    Add files via upload

---
+++ day1.sql
@@ -0,0 +1,10 @@
+use amrita;
+
+create table emp(
+id int(5) primary key,
+name varchar(50),
+salary int(10),
+location varchar(100)
+);
+
+select * from emp;
---
+++ day2.sql
@@ -0,0 +1,18 @@
+use amrita;
+
+select * from emp;
+
+insert into emp(id,name,salary,location) values 
+(1,'amrita',150000,'churchgate'),
+(2,'ayushee',100000,'kandivali'),
+(3,'ankita',120000,'malad'),
+(4,'ananya',140000,'vasai road');
+
+-- update table
+update emp set salary = 170000 where id=2;
+
+-- add column
+alter table emp add column gender varchar(10);
+
+-- delete column
+alter table emp drop column gender;
